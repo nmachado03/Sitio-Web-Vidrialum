c08a8f54550ce809b56617debd308ea0ee1644ff
Ezequiel-Padilla-261 <theeze216@gmail.com>
2019-12-10 17:28:25 -0300

    home

--- js/main.js
+++ js/main.js
@@ -1,21 +1,19 @@
-$(document).ready(main);
-
-var contador = 1;
-
-function main(){
-	$('.menu_bar').click(function(){
-		//$('nav').toggle(); 
-
-		if(contador == 1){
-			$('nav').animate({
-				top: '0'
-			});
-			contador = 0;
-		} else {
-			contador = 1;
-			$('nav').animate({
-				top: '-100%'
-			});
-		}
-	});
-};
+$(document).ready(function() {
+  $("#menu_bar").click(function() {
+    $("body").toggleClass("visible_menu");
+  });
+  $(window).scroll(function() {
+    let sc = $(window).scrollTop();
+    if (sc > 500) {
+      $(".header").addClass("header_2");
+      $(".btn-header").addClass("btn-header_2");
+      $(".logo").addClass("logo_2");
+      $(".menu_hamburger").addClass("menu_hamburger_2");
+    } else {
+      $(".header").removeClass("header_2");
+      $(".btn-header").removeClass("btn-header_2");
+      $(".logo").removeClass("logo_2");
+      $(".menu_hamburger").removeClass("menu_hamburger_2");
+    }
+  });
+});
